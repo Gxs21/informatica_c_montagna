d4acd57e6870f7121653920dd77ee275db72439b
Giulo <giulio.sodero4@gmail.com>
2025-04-09 13:00:12 +0200

    add new line

---
+++ esercitazione_array_6/esercizio_1.c
@@ -0,0 +1,8 @@
+/* Si realizzi una funzione
+
+void prompt_array(int array[], int size);
+che chieda all'utente di inserire una quantità di interi pari a size. La funzione deve disporre gli interi forniti dall'utente all'interno dell'array fornito come parametro, nello stesso ordine in cui sono forniti dall'utente. Si assuma che l'array sia stato allocato dal chiamate ad una dimensione (almeno) pari a size.
+
+Il main di prova può chiedere a sua volta la dimensione dell'array (size) all'utente. */
+
+void prompt_array(int array[], int size);
