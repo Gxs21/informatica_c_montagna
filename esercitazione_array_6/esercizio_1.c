d9500317f9f7a2ae9fe1db743925ce7ddd6d3e87
Giulo <giulio.sodero4@gmail.com>
2025-04-14 11:54:25 +0200

    add new line

--- esercitazione_array_6/esercizio_1.c
+++ esercitazione_array_6/esercizio_1.c
@@ -1,8 +1,35 @@
 /* Si realizzi una funzione
 
 void prompt_array(int array[], int size);
-che chieda all'utente di inserire una quantità di interi pari a size. La funzione deve disporre gli interi forniti dall'utente all'interno dell'array fornito come parametro, nello stesso ordine in cui sono forniti dall'utente. Si assuma che l'array sia stato allocato dal chiamate ad una dimensione (almeno) pari a size.
+che chieda all'utente di inserire una quantità di interi pari a size. 
+La funzione deve disporre gli interi forniti dall'utente all'interno dell'array fornito 
+come parametro, nello stesso ordine in cui sono forniti dall'utente. Si assuma che l'array 
+sia stato allocato dal chiamate ad una dimensione (almeno) pari a size.
 
 Il main di prova può chiedere a sua volta la dimensione dell'array (size) all'utente. */
-
-void prompt_array(int array[], int size);
+#include <stdio.h>
+#include <stdlib.h>
+void prompt_array(int array[], int size){
+    for (int i=0; i<size; i++){
+        printf("inserire un numero intero\n");
+        scanf("%d", &array[i]);
+        printf("%d \n", array[i]);
+    }
+    for(int i=0; i<size; i++){
+        printf("\n %d ", array[i]);
+    }
+}
+int main(){
+    int size=0;
+    printf("inserire quanti interi si vogliono inserire:\n");
+    scanf("%d", &size);
+    int array[size];
+    prompt_array(array, size);
+    for(int i=0; i<size; i++){
+        printf("\n %d ", array[i]);
+    
+    }
+    return 0;
+}
+//chiedigli come funziona il paddaggio del parametro da riempire per non avere un ritorno di tipo array
+//chiedi perchè non puoi usare la sintassi che hai usato 
